d59ac9c41a991fca1d7641f45ea92ed69d5ae663
Joseph Luck <joseph.luck@sky.com>
2017-11-06 08:50:20 +0000

    Moar tests

---
+++ tests/apps/component-lifecycle-mount.ts
@@ -0,0 +1,51 @@
+import { View, h, app, Component } from '../../src'
+
+window['counts'] = {
+  render: 0,
+  componentRender: 0,
+  onBeforeMount: 0,
+  onAfterMount: 0,
+}
+
+
+const component = (count: number): Component<{}> => {
+  return {
+    state: {},
+    onBeforeMount() {
+      window['counts'].onBeforeMount++
+    },
+    onAfterMount() {
+      window['counts'].onAfterMount++
+    },
+    render() {
+      window['counts'].componentRender++
+      return h('p', { id: 'component' }, [
+        h('span', { id: 'component-onBeforeMount-count' }, window['counts'].onBeforeMount),
+        h('span', { id: 'component-onAfterMount-count' }, window['counts'].onAfterMount),
+        h('span', { id: 'component-render-count' }, window['counts'].render),
+        'Ahh!'
+      ])
+    }
+  }
+}
+
+export type State = {
+  count: number
+}
+
+const defaultState: State = { count: 0 }
+
+const view: View<State> = (state = defaultState, update) => {
+  window['counts'].render++
+  const increment = () => update({ count: state.count + 1 })
+  return h('div', {}, [
+    h('span', { id: 'count' }, state.count),
+    h('button', { id: 'increment-button', onclick: increment }, 'Increment'),
+    component(state.count)
+  ])
+}
+
+const node = document.createElement('div')
+document.body.appendChild(node)
+const { run } = app(defaultState)
+run(node, view)
